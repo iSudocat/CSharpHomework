30cc4ccf4ae8fe7d14f9fbe3e241cb7c18950fd0
isudocat <i@sudocat.me>
2020-02-21 16:47:54 +0800

    2020.2.21

---
+++ 窗口计算器/窗口计算器/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace 窗口计算器
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_num1 = new System.Windows.Forms.TextBox();
+            this.textBox_num2 = new System.Windows.Forms.TextBox();
+            this.button_cul = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.comboBox1.Location = new System.Drawing.Point(91, 104);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 29);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(27, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "运算符";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(27, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "数字1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(27, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 21);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "数字2";
+            // 
+            // textBox_num1
+            // 
+            this.textBox_num1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox_num1.Location = new System.Drawing.Point(91, 21);
+            this.textBox_num1.Name = "textBox_num1";
+            this.textBox_num1.Size = new System.Drawing.Size(121, 29);
+            this.textBox_num1.TabIndex = 4;
+            // 
+            // textBox_num2
+            // 
+            this.textBox_num2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox_num2.Location = new System.Drawing.Point(91, 62);
+            this.textBox_num2.Name = "textBox_num2";
+            this.textBox_num2.Size = new System.Drawing.Size(121, 29);
+            this.textBox_num2.TabIndex = 5;
+            // 
+            // button_cul
+            // 
+            this.button_cul.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_cul.Location = new System.Drawing.Point(31, 153);
+            this.button_cul.Name = "button_cul";
+            this.button_cul.Size = new System.Drawing.Size(181, 29);
+            this.button_cul.TabIndex = 6;
+            this.button_cul.Text = "计算";
+            this.button_cul.UseVisualStyleBackColor = true;
+            this.button_cul.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(27, 197);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 21);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "计算结果：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 257);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button_cul);
+            this.Controls.Add(this.textBox_num2);
+            this.Controls.Add(this.textBox_num1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_num1;
+        private System.Windows.Forms.TextBox textBox_num2;
+        private System.Windows.Forms.Button button_cul;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ 窗口计算器/窗口计算器/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 窗口计算器
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+        }
+        private void Add(double a, double b)
+        {
+            double result = a + b;
+            label4.Text = "计算结果：" + Convert.ToString(result);
+        }
+        private void Minus(double a, double b)
+        {
+            double result = a - b;
+            label4.Text = "计算结果：" + Convert.ToString(result);
+        }
+        private void Multiply(double a, double b)
+        {
+            double result = a * b;
+            label4.Text = "计算结果：" + Convert.ToString(result);
+        }
+        private void Divide(double a, double b)
+        {
+            double result = a / b;
+            label4.Text = "计算结果：" + Convert.ToString(result);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double n1 = 0, n2 = 0;
+            try
+            {
+                n1 = Convert.ToDouble(textBox_num1.Text);
+                n2 = Convert.ToDouble(textBox_num2.Text);
+                switch (comboBox1.SelectedItem.ToString())
+                {
+                    case "+":
+                        Add(n1, n2);
+                        break;
+                    case "-":
+                        Minus(n1, n2);
+                        break;
+                    case "*":
+                        Multiply(n1, n2);
+                        break;
+                    case "/":
+                        Divide(n1, n2);
+                        break;
+                }
+            }
+            catch(FormatException)
+            {
+               label4.Text = "错误的数字";
+               return;
+            }
+            catch (DivideByZeroException)   //double除0不会产生此异常，故其实不需要
+            {
+               label4.Text = "不能除以0";
+            }
+            catch (OverflowException)
+            {
+                label4.Text = "数据溢出";
+            }
+        }
+    }
+}
