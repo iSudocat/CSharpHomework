bd368580219cbea6218a7d7f990124f1774ee55e
isudocat <i@sudocat.me>
2020-03-13 23:49:34 +0800

    2020.3.13

---
+++ Week 4/泛型链表/泛型链表/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+/*******************************题目***********************************
+1、为示例中的泛型链表类添加类似于List<T>类的ForEach(Action<T> action)方法。
+通过调用这个方法打印链表元素，求最大值、最小值和求和（使用lambda表达式实现）。
+**********************************************************************/
+
+namespace 泛型链表
+{
+    public class Node<T>    // 链表节点
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+    public class GenericList<T>    //泛型链表类
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        public GenericList()
+        {
+            tail = head = null;
+        }
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            Node<T> node = this.head;
+            while (node != null)
+            {
+                action(node.Data);
+                node = node.Next;
+            }
+
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> intlist = new GenericList<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                intlist.Add(x);
+            }
+
+            intlist.ForEach(m => Console.WriteLine(m));
+
+            int max = int.MinValue;
+            intlist.ForEach(m => { if (m > max) max = m; });
+            Console.WriteLine("最大值：" + max);
+
+            int min = int.MaxValue;
+            intlist.ForEach(m => { if (m < min) min = m; });
+            Console.WriteLine("最小值：" + min);
+
+            int sum = 0;
+            intlist.ForEach(m =>  sum += m);
+            Console.WriteLine("和：" + sum);
+        }
+    }
+}
---
+++ Week 4/闹钟/闹钟/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+/******************************题目****************************
+2、使用事件机制，模拟实现一个闹钟功能。闹钟可以有嘀嗒（Tick）事件和
+响铃（Alarm）两个事件。在闹钟走时时或者响铃时，在控制台显示提示信息。
+**************************************************************/
+
+namespace 闹钟
+{
+    public class Clock
+    {
+
+        public int Hour { get; set; }       //当前时
+        public int Minute { get; set; }     //当前分
+        public int Second { get; set; }     //当前秒
+        public bool IfAlarm { get; set; }     //是否设定响铃时间
+        public int AlarmHour { get; set; }      //响铃时
+        public int AlarmMinute { get; set; }        //响铃分
+        public int AlarmSecond { get; set; }        //响铃秒
+
+        public delegate void TickHandler();
+        public event TickHandler OnTick;
+        public event TickHandler OnAlarm;
+
+        public void Tick()
+        {
+            OnTick();
+        }
+        public void Alarm()
+        {
+            if (AlarmHour == Hour && AlarmMinute == Minute && AlarmSecond == Second)
+                OnAlarm();
+        }
+
+        public void Update()
+        {
+            Second++;
+            if(Second==60)
+            {
+                Second = 0;
+                Minute++;
+                if(Minute==60)
+                {
+                    Minute = 0;
+                    Hour++;
+                    if(Hour==24)
+                    {
+                        Hour = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static Clock clock = new Clock();
+        static void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            clock.Tick();
+            clock.Alarm();
+            clock.Update();
+        }
+        static void Main(string[] args)
+        {
+            clock.OnTick += Tick;
+            clock.OnAlarm += Alarm;
+            void Tick()
+            {
+                Console.WriteLine($"Ring……当前时间：{clock.Hour}:{clock.Minute}:{clock.Second}");
+            }
+            void Alarm()
+            {
+                Console.WriteLine($"Alarm……当前时间：{clock.Hour}:{clock.Minute}:{clock.Second}");
+            }
+
+            clock.Hour = 23;
+            clock.Minute = 58;
+            clock.Second = 0;
+            clock.AlarmHour = 23;
+            clock.AlarmMinute = 59;
+            clock.AlarmSecond = 58;
+
+            System.Timers.Timer timer;
+            timer = new System.Timers.Timer(1000);
+            timer.Elapsed += OnTimedEvent;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+
+            Console.ReadLine();
+        }
+    }
+}
