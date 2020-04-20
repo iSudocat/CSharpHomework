71cf8f1ff36a03c7543347c5114efaccce74a44b
isudocat <i@sudocat.me>
2020-04-20 19:41:13 +0800

    2020.4.17

---
+++ Week 9/爬虫 - 控制台/爬虫 - 控制台/SimpleCrawler.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleCrawler
+{
+    class SimpleCrawler
+    {
+        private Hashtable urls = new Hashtable();
+        private int count = 0;
+        static private string startUrl = "";
+        static void Main(string[] args)
+        {
+            SimpleCrawler myCrawler = new SimpleCrawler();
+            startUrl = "https://www.cnblogs.com/dstang2000/";
+            if (args.Length >= 1) startUrl = args[0];
+            myCrawler.urls.Add(startUrl, false);//加入初始页面
+            new Thread(myCrawler.Crawl).Start();
+        }
+
+        private void Crawl()
+        {
+            Console.WriteLine("开始爬行了.... ");
+            while (true)
+            {
+                string current = null;
+                foreach (string url in urls.Keys)
+                {
+                    if ((bool)urls[url]) continue;
+                    current = url;
+                }
+
+                if (current == null || count > 10) break;
+                Console.WriteLine("爬行" + current + "页面!");
+                string html = DownLoad(current); // 下载
+                urls[current] = true;
+                count++;
+                Parse(html, current);//解析,并加入新的链接
+                Console.WriteLine("爬行结束");
+            }
+            Console.ReadLine();
+        }
+
+        public string DownLoad(string url)
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.Encoding = Encoding.UTF8;
+                string html = webClient.DownloadString(url);
+                string fileName = count.ToString();
+                File.WriteAllText(fileName, html, Encoding.UTF8);
+                return html;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return "";
+            }
+        }
+
+        private void Parse(string html, string oldUrl)
+        {
+
+            //匹配不含相对路径,且包含html的网址
+            string strRef = @"(href|HREF)[ ]*=[ ]*[""'](http|https)[^""'#>]+..html.*?[""']";
+            MatchCollection matches = new Regex(strRef).Matches(html);
+            foreach (Match match in matches)
+            {
+                var url = match.Value.Substring(match.Value.IndexOf('=') + 1).Trim('"', '\"', '#', '>');
+                if (url.Length == 0)
+                    continue;
+                //仅包含起始网站上的网页
+                if (url.Contains("https://www.cnblogs.com"))
+                {
+                    if (urls[url] == null)
+                        urls[url] = false;
+                }
+            }
+
+            //匹配相对路径,且包含html的网址
+
+            strRef = @"(href|HREF)[ ]*=[ ]*[""'][^(http|https)][^""'#>]+..html.*?[""']";
+            matches = new Regex(strRef).Matches(html);
+            foreach (Match match in matches)
+            {
+                var url = match.Value.Substring(match.Value.IndexOf('=') + 1).Trim('"', '\"', '#', '>');
+                if (url.Length == 0) continue;
+                Uri baseUri = new Uri(oldUrl);
+                Uri absoluteUri = new Uri(baseUri,url);
+                Console.WriteLine("相对:" + url);
+                Console.WriteLine("绝对:" + absoluteUri.ToString());
+                if (urls[absoluteUri.ToString()] == null)
+                    urls[absoluteUri.ToString()] = false;
+            }
+        }
+    }
+}
