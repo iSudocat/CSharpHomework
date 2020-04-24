bd072d2b9178d08c502cecefce55e4c2dde4d5c3
isudocat <i@sudocat.me>
2020-04-24 18:50:17 +0800

    2020.4.17

---
+++ Week 9/爬虫 - Winform/Crawler - Winform/Form1.cs
@@ -0,0 +1,58 @@
+﻿using SimpleCrawler;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace Crawler___Winform
+{
+    public partial class Form1 : Form
+    {
+        BindingSource resultBindingSource = new BindingSource();
+        Crawler Crawler = new Crawler();
+        Thread thread = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ResultGridView.DataSource = resultBindingSource;
+            Crawler.PageDownloaded += PageDownloaded;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            resultBindingSource.Clear();
+            Crawler.startUrl = txtUrl.Text;
+
+            if (thread != null)
+            {
+                thread.Abort();
+            }
+            Crawler.urls.Add(Crawler.startUrl, false);//加入初始页面
+            thread = new Thread(Crawler.Crawl);
+            thread.Start();
+        }
+
+
+        private void PageDownloaded(Crawler crawler, string url)
+        {
+            var pageInfo = new { Index = resultBindingSource.Count + 1, URL = url };
+            Action action = () => { resultBindingSource.Add(pageInfo); };
+            if (this.InvokeRequired)
+            {
+                this.Invoke(action);
+            }
+            else
+            {
+                action();
+            }
+        }
+    }
+}
---
+++ Week 9/爬虫 - Winform/Crawler - Winform/SimpleCrawler.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleCrawler
+{
+    class Crawler
+    {
+        public event Action<Crawler, string> PageDownloaded;
+
+        public Hashtable urls = new Hashtable();
+        private int count = 0;
+        static public string startUrl = "";
+        static public string startWith = "";
+
+        public void Crawl()
+        {
+            string str = @"(www\.){0,1}.*?\..*?/";
+            Regex r = new Regex(str);
+            Match m = r.Match(startUrl);
+            startWith = m.Value;
+
+            //Console.WriteLine("开始爬行了.... ");
+            while (true)
+            {
+                string current = null;
+                foreach (string url in urls.Keys)
+                {
+                    if ((bool)urls[url]) continue;
+                    current = url;
+                }
+
+                if (current == null || count > 20) break;
+                //Console.WriteLine("爬行" + current + "页面!");
+                string html = DownLoad(current); // 下载
+                urls[current] = true;
+                count++;
+                PageDownloaded(this, current);
+                Parse(html, current);//解析,并加入新的链接
+                //Console.WriteLine("爬行结束");
+            }
+            //Console.ReadLine();
+        }
+
+        public string DownLoad(string url)
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.Encoding = Encoding.UTF8;
+                string html = webClient.DownloadString(url);
+                string fileName = count.ToString();
+                File.WriteAllText(fileName, html, Encoding.UTF8);
+                return html;
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine(ex.Message);
+                return "";
+            }
+        }
+
+        private void Parse(string html, string oldUrl)
+        {
+
+            //匹配不含相对路径,且包含html的网址
+            string strRef = @"(href|HREF)[ ]*=[ ]*[""'](http|https)[^""'#>]+..html.*?[""']";
+            MatchCollection matches = new Regex(strRef).Matches(html);
+            foreach (Match match in matches)
+            {
+                var url = match.Value.Substring(match.Value.IndexOf('=') + 1).Trim('"', '\"', '#', '>');
+                if (url.Length == 0)
+                    continue;
+                //仅包含起始网站上的网页
+                if (url.Contains(startWith))
+                {
+                    if (urls[url] == null)
+                        urls[url] = false;
+                }
+            }
+
+            //匹配相对路径,且包含html的网址
+
+            strRef = @"(href|HREF)[ ]*=[ ]*[""'][^(http|https)][^""'#>]+..html.*?[""']";
+            matches = new Regex(strRef).Matches(html);
+            foreach (Match match in matches)
+            {
+                var url = match.Value.Substring(match.Value.IndexOf('=') + 1).Trim('"', '\"', '#', '>');
+                if (url.Length == 0) continue;
+                Uri baseUri = new Uri(oldUrl);
+                Uri absoluteUri = new Uri(baseUri,url);
+                Console.WriteLine("相对:" + url);
+                Console.WriteLine("绝对:" + absoluteUri.ToString());
+                if (urls[absoluteUri.ToString()] == null)
+                    urls[absoluteUri.ToString()] = false;
+            }
+        }
+    }
+}
