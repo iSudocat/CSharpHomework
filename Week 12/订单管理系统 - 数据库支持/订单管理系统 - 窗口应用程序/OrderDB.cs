ffe3a9ae633dbda83dd00b27082ff84be4ec7bb9
isudocat <i@sudocat.me>
2020-05-15 22:30:18 +0800

    2020.5.8 with 2020.4.10（同时补充了对排序模块的调用）

---
+++ Week 12/订单管理系统 - 数据库支持/订单管理系统 - 窗口应用程序/OrderDB.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OrderDB
+{
+    public class Order
+    {
+
+        [Required]
+        public string Customer { get; set; }
+        [Key]
+        public string ID { get; set; }
+        public double TotalPrice { get; set; }
+
+        public List<Item> Items { get; set; }   //一对多关联
+    }
+
+    public class Item
+    {
+
+        [Required]
+        [Key, Column(Order = 2)]
+        public string Pname { get; set; }   //商品名
+        [Required]
+        public int Pnum { get; set; }   //商品数量
+        [Required]
+        public double PPrice { get; set; }   //单个商品总价
+
+        [Required]
+        [Key, Column(Order = 1)]
+        public string ID { get; set; }
+        [ForeignKey("ID")]
+        public Order Order { get; set; }    //多对一关联
+    }
+
+}
